b563945add3cd2a481c3918ecf8495fa8694868c
ma137 <ma8719670@gmail.com>
2026-05-23 20:18:55 +0700

    Form Validation

--- BTH_03/QLSV.js
+++ BTH_03/QLSV.js
@@ -104,6 +104,7 @@ function showMessage(text){
 
 document.getElementById('form-js').addEventListener('submit', function(event) {
     event.preventDefault();
+    var isValid = true;
     var msv = document.getElementById('msv2');
     var name = document.getElementById('name2');
     var dob = document.getElementById('dbt2');
@@ -114,26 +115,104 @@ document.getElementById('form-js').addEventListener('submit', function(event) {
     var cl = document.getElementById('cl2');
 
     document.querySelectorAll('.error').forEach(e => e.textContent = '');
-    const student = {
-        msv: msv.value.trim(),
-        name: name.value.trim(),
-        dob: dob.value,
-        cl: cl.value,
-        dtb: dtb.value,
-        email: email.value.trim(),
-        password: password.value
-    };
-    if(editIndex === -1){
-        students.push(student);
-        showMessage("Thêm sinh viên thành công!");
-    }else{
-        students[editIndex] = student;
-        showMessage("Cập nhật thành công!");
-        editIndex = -1;
+
+    if (!msv.value.trim()){
+        isValid = false;
+        document.getElementById('error-msv2').textContent = 'Không được để trống';
     }
-    saveStudents();
-    renderStudents();
-    closeForm();
+    else if (!/^SV\d{6}$/.test(msv.value.trim())) {
+        isValid = false;
+        document.getElementById('error-msv2').textContent = 'MSV phải dạng SV123456';
+    }
+
+    if (!name.value.trim()) {
+        isValid = false;
+        document.getElementById('error-name2').textContent = 'Không được để trống';
+    }
+    else if (!/^[A-Za-zÀ-ỹ\s]+$/.test(name.value.trim())) {
+        isValid = false;
+        document.getElementById('error-name2').textContent = 'Chỉ chứa chữ và khoảng trắng';
+    }
+
+    if (!dob.value) {
+        isValid = false;
+        document.getElementById('error-dbt2').textContent = 'Không được để trống';
+    } else {
+        var birth = new Date(dob.value);
+        var today = new Date();
+        var age = today.getFullYear() - birth.getFullYear();
+
+        if (age < 18) {
+            isValid = false;
+            document.getElementById('error-dbt2').textContent = 'Phải đủ 18 tuổi';
+        }
+    }
+
+    if (!cl.value) {
+        isValid = false;
+        document.getElementById('error-cl2').textContent = 'Phải chọn lớp';
+    }
+
+    if (!dtb.value.trim()) {
+        isValid = false;
+        document.getElementById('error-dtb2').textContent = 'Không được để trống';
+    }
+    else if (!/^\d+(\.\d{1,2})?$/.test(dtb.value) || dtb.value < 0 || dtb.value > 10) {
+        isValid = false;
+        document.getElementById('error-dtb2').textContent = 'Từ 0-10, tối đa 2 số thập phân';
+    }
+
+    if (!email.value.trim()) {
+        isValid = false;
+        document.getElementById('error-email2').textContent = 'Không được để trống';
+    }
+    else if (!/^[\w.-]+@student\.edu\.vn$/.test(email.value.trim())) {
+        isValid = false;
+        document.getElementById('error-email2').textContent = 'Email phải @student.edu.vn';
+    }
+
+    if (!password.value.trim()) {
+        isValid = false;
+        document.getElementById('error-password2').textContent = 'Không được để trống';
+    }
+    else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(password.value)) {
+        isValid = false;
+        document.getElementById('error-password2').textContent =
+            'Ít nhất 8 ký tự, có hoa, thường, số, ký tự đặc biệt';
+    }
+
+    if (!pw.value.trim()) {
+        isValid = false;
+        document.getElementById('error-pw2')
+        .textContent = 'Không được để trống';
+    }
+    else if (pw.value !== password.value) {
+        isValid = false;
+        document.getElementById('error-pw2').textContent = 'Không khớp mật khẩu';
+    }
+
+    if(isValid){
+        const student = {
+            msv: msv.value.trim(),
+            name: name.value.trim(),
+            dob: dob.value,
+            cl: cl.value,
+            dtb: dtb.value,
+            email: email.value.trim(),
+            password: password.value
+        };
+        if(editIndex === -1){
+            students.push(student);
+            showMessage("Thêm sinh viên thành công!");
+        }else{
+            students[editIndex] = student;
+            showMessage("Cập nhật thành công!");
+            editIndex = -1;
+        }
+        saveStudents();
+        renderStudents();
+        closeForm();
+    }
 });
 
 function editStudent(index){
