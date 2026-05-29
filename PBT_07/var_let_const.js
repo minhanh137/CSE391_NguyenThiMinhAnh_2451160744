a5850b037888805d038f9e7b074880d257130819
ma137 <ma8719670@gmail.com>
2026-05-30 01:26:52 +0700

    Sua cau A1 + Xong bai B1

--- PBT_07/var_let_const.js
+++ PBT_07/var_let_const.js
@@ -1,26 +1,43 @@
 // Đoạn 1 => undefined
-console.log(x);
-var x = 5;
+try {
+    console.log(x);
+    var x = 5;
+} catch(e) {
+        console.error("-", e.message);
+}
 
-// Đoạn 2 => undefined
-console.log(y);
-let y = 10;
+// Đoạn 2 => Sẽ in ra lỗi ReferenceError
+try {
+    console.log(y);
+    let y = 10;
+} catch(e) {
+        console.error("-", e.message);
+}
 
-// Đoạn 3 => Cannot assign to "z" because it is a constant
-const z = 15;
-z = 20;
-console.log(z);
+// Đoạn 3 => Sẽ in ra lỗi TypeError
+try {
+    const z = 15;
+    z = 20;
+    console.log(z);
+} catch(e) {
+        console.error("-", e.message);
+}
 
 // Đoạn 4 => (4) [1, 2, 3, 4]
-const arr = [1, 2, 3];
-arr.push(4);
-console.log(arr);
+try {
+    const arr = [1, 2, 3];
+    arr.push(4);
+    console.log(arr);
+} catch(e) {
+        console.error("-", e.message);}
 
-// Đoạn 5 => Trong block: 2
-//           Ngoài block: 1
-let a = 1;
-{
-    let a = 2;
-    console.log("Trong block:", a);
-}
-console.log("Ngoài block:", a);
+// Đoạn 5 => Trong block: 2 / Ngoài block: 1
+try {
+    let a = 1;
+    {
+        let a = 2;
+        console.log("Trong block:", a);
+    }
+    console.log("Ngoài block:", a);
+} catch(e) {
+        console.error("-", e.message);}
